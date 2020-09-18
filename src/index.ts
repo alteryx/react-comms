5612831c3d8c8ca4acf850453fdc6a69f28f4853
Jordan Quinn <jordan.quinn@alteryx.com>
2020-09-18 08:44:36 -0600

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { default as Context } from './Context';
-export { default as DesignerMessageApi } from './DesignerMessageApi';
-export { default as Provider } from './Provider';
+export { default as DesignerApi } from './DesignerApi';
