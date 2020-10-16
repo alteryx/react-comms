cd40d208e4e79498a6869457dd9d244a07a161fb
Jordan Quinn <jordan.quinn@alteryx.com>
2020-10-16 12:51:20 -0600

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { default as Context } from './Context';
 export { default as DesignerApi } from './DesignerApi';
+export { default as DesignerMessageApi } from './DesignerMessageApi';
+export { JsEvent } from './Utils/callback';
+export { default as MessageApiBase } from './MessageApiBase';
